d589f37b058df85dc94a40f73627853ea2f65f4d
Renu <100067@bq-067.bambeeq.com>
2018-11-12 16:02:00 +0530

    Offline API change-event to flow via kafka not eventbus

--- src/main/java/org/gooru/nucleus/insights/events/gateway/routes/RouteEventsWriteConfigurator.java
+++ src/main/java/org/gooru/nucleus/insights/events/gateway/routes/RouteEventsWriteConfigurator.java
@@ -1,6 +1,5 @@
 package org.gooru.nucleus.insights.events.gateway.routes;
 
-import org.gooru.nucleus.insights.events.gateway.bootstrap.BootstrapVerticle;
 import org.gooru.nucleus.insights.events.gateway.constants.ConfigConstants;
 import org.gooru.nucleus.insights.events.gateway.constants.MessageConstants;
 import org.gooru.nucleus.insights.events.gateway.constants.MessagebusEndpoints;
@@ -80,14 +79,19 @@ class RouteEventsWriteConfigurator implements RouteConfigurator {
               options, reply -> new RouteResponseUtility().responseHandler(routingContext, reply, LOGGER));      
     });
     
-    mbusTimeout = config.getLong(ConfigConstants.MBUS_TIMEOUT, 30L) * 1000;
     router.post(RouteConstants.OFFLINE_REPORT_POST).handler(routingContext -> {
-      DeliveryOptions options = new DeliveryOptions().setSendTimeout(mbusTimeout * 1000).addHeader(MessageConstants.MSG_HEADER_OP,
-              MessageConstants.MSG_OP_OFFLINE_REPORT);
-      JsonObject request = new RouteRequestUtility().getJObjectBodyForMessage(routingContext);
-      eb.send(MessagebusEndpoints.MBEP_ANALYTICS_OFFLINE_REPORT, request,
-              options, reply -> new RouteResponseUtility().responseHandler(routingContext, reply, LOGGER));      
-    });
+        JsonObject request = new RouteRequestUtility().getJObjectBodyForMessage(routingContext);  
+        LOGGER.debug("REQUEST ::: {} ", request);        
+        JsonObject eventObj = request.getJsonObject(MessageConstants.MSG_HTTP_BODY);
+        if (eventObj != null && !eventObj.isEmpty()) {
+            eventObj.put(ConfigConstants._EVENT_NAME, ConfigConstants.OFFLINE_STUDENT_EVENT);
+            kafkaMessageProducer(eventObj); 
+            routingContext.response().setStatusCode(200).end();
+        } else {
+          // Event Object is mandatory.
+          routingContext.response().setStatusCode(400).end();
+        }
+      });
 
   } // End Configure Routes
 
